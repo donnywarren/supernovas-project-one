5b2781615321b4316edf96be84a576513a5af856
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-10 18:58:03 -0400

    connecting pages

--- script.js
+++ script.js
@@ -1,4 +1,94 @@
 
+openFactPage = () => {
+  window.location = "fact-page.html";
+}
+
+openTrailerPage = () => {
+  window.location = "trailer-page.html";
+}
+
+openHomePage = () => {
+  window.location = "index.html";
+}
+
+
+const factPageButton1 = document.querySelector("#poster1");
+
+factPageButton1.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton2 = document.querySelector("#poster2");
+
+factPageButton2.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton3 = document.querySelector("#poster3");
+
+factPageButton3.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton4 = document.querySelector("#poster4");
+
+factPageButton4.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton5 = document.querySelector("#poster5");
+
+factPageButton5.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton6 = document.querySelector("#poster6");
+
+factPageButton6.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton7 = document.querySelector("#poster7");
+
+factPageButton7.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton8 = document.querySelector("#poster8");
+
+factPageButton8.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+const factPageButton9 = document.querySelector("#poster9");
+
+factPageButton9.addEventListener('click', function () {
+
+  openFactPage();
+});
+
+
+
+
+// const factPageButton = document.querySelector(".poster7-container");
+
+// factPageButton.addEventListener('click', function () {
+//   // let changeAttribute = document.querySelector("#trailer-last-jedi");
+//   // changeAttribute.setAttribute("style", "initial");
+//   openFactPage();
+// });
+
+
+
+
 // const title = document.querySelector(".display1");
 // const overview = document.querySelector(".display2");
 // const posterUrl = document.querySelector(".display3");
@@ -36,4 +126,18 @@
 // };
 
 // apiTester();
+
+
+
+// const trailerHomeButton = document.querySelector("#home-btn");
+
+// trailerHomeButton.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   let changeAttribute = document.querySelector("#trailer-last-jedi");
+//   changeAttribute.setAttribute("style", "initial");
+
+// });
+
+
+
 
