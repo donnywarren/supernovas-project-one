ecac0e0ae7876bcd28a2673aaf3973c5612bad37
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-15 14:25:55 -0400

    star background

--- people.js
+++ people.js
@@ -3,9 +3,20 @@ openHomePage = () => {
   window.location = "home-page.html";
 }
 
-
 const homeButton = document.querySelector(".home-btn");
 
 homeButton.addEventListener('click', function () {
   openHomePage();
+});
+
+
+
+backOnePage = () => {
+  window.history.back();
+}
+
+const backButton = document.querySelector(".back-btn");
+
+backButton.addEventListener('click', function () {
+  backOnePage();
 });
